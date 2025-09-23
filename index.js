761b9ea8a678ef05b9ffe555686c28c91a4ec8b4
Paras Mahajan <parasmahajan1010@gmail.com>
2025-09-23 16:37:34 +0530

    DONE WITH PRACTICE FROM JAVASCRIP.INFO/ARRAY-METHODS

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // user.surname = 'Smith';
 // console.log(user);
 
+
 // user.name = "pete";
 
 // console.log(user);
@@ -79,31 +80,138 @@
 // databaseOfUser.login();
 
 
-function Person(name) {
-    this.name =  name;
-    this.introduceSelf =  function () {
-        console.log(`HI, this side ${this.name} here!`)
+// function Person(name) {
+//     this.name =  name;
+//     this.introduceSelf =  function () {
+//         console.log(`HI, this side ${this.name} here!`)
+//     };
+// };
+
+// const salva = new Person("Salva");
+// console.log(salva);
+
+// salva.introduceSelf();
+
+// const frankie = new Person("frankie");
+// console.log(frankie);
+// frankie.introduceSelf();
+
+// const notification = new Notification("Hello");
+
+// let data = 42;
+// let datacopy = data;
+// console.log(data);
+// console.log(datacopy);
+
+// datacopy = 43;
+
+// console.log(datacopy);
+// console.log(data);
+
+// const obj = {data: 42};
+// const objCopy = obj;
+
+// console.log(obj);
+// objCopy.data = 43;
+
+// console.log(obj);
+// console.log(objCopy);
+
+// let check = {
+//     fruit: "apple",
+// };
+
+// console.log(check);
+
+// let checkCopy = check;
+
+// console.log(checkCopy);
+
+// checkCopy = {fruit: "Orange"};
+
+// console.log(check);
+// console.log(checkCopy);
+
+
+// let john = {name: "John", age: 25};
+// let pete = {name: "Pete", age: 30};
+// let mary = {name: "Mary", age: 28};
+
+// let users = [john, pete, mary];
+
+// function writingNames(arr) {
+//     let names = [];
+//     for (let person of arr) {
+//         names.push(person.name);
+//     };
+//     return names;
+// };
+// console.log(writingNames(users));
+
+// let names = users.map((user) => {
+//     return user.name;
+// });
+// console.log(names);
+
+// let john = { name: "John", surname: "Smith", id: 1 };
+// let pete = { name: "Pete", surname: "Hunt", id: 2 };
+// let mary = { name: "Mary", surname: "Key", id: 3 };
+
+// let users = [john, pete, mary];
+
+// let usersMapped = users.map((user) => {
+//     let newObj = {}
+//     newObj.fullName = user.name + " "+ user.surname;
+//     newObj.id = user.id;
+//     return newObj;
+// });
+
+// console.log(usersMapped);
+
+// alert(usersMapped[0].id);
+// alert(usersMapped[0].fullName);
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 29 };
+
+// let arr = [john, pete, mary];
+
+// function sortByAge(arr) {
+//     return arr.sort((a,b) => a.age - b.age)
+// }
+// console.log(sortByAge(arr));
+
+// function getAverageAge(arr) {
+//     let sumValue = 0;
+//     for (let i=0; i<arr.length; i++) {
+//         sumValue += arr[i].age;
+//     };
+//     return sumValue/(arr.length)
+// };
+
+// console.log(getAverageAge(arr));
+
+let users = [
+  {id: 'john', name: "John Smith", age: 20},
+  {id: 'ann', name: "Ann Smith", age: 24},
+  {id: 'pete', name: "Pete Peterson", age: 31},
+];
+function usersById(users) {
+    let newObj = {};
+    for (let person of users) {
+        newObj[person.id] = person;
     };
+    return newObj;
 };
+let usersById2 = usersById(users);
+console.log(usersById2);
+let emptyObj = {}
+let xyz = users.reduce((obj, user) => {obj[user.id] = user;
+    return obj
+}, {});
 
-const salva = new Person("Salva");
-console.log(salva);
-
-salva.introduceSelf();
-
-const frankie = new Person("frankie");
-console.log(frankie);
-frankie.introduceSelf();
-
-const notification = new Notification("Hello");
-
-
-
-
-
-
-
-
+console.log(xyz)
 
 
 
